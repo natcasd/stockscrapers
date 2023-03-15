03edefb6ee9759a2b697034889b7b0064982e4f0
car-losb <77890255+car-losb@users.noreply.github.com>
2023-03-14 22:08:56 -0400

    pandas df to csv

--- yahoo_stocks.py
+++ yahoo_stocks.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sqlite3
+from sqlalchemy import create_engine
 
 
 import yfinance as yf
 
-
+#pull history data for stocks
 df = pd.DataFrame()
 securities = ['msft', 'aapl', 'tsla', 'amzn', 'meta', 'brk-b', 'goog', 'jnj', 'jpm', 'v', 'pg',
              'ma', 'intc', 'unh', 'bac', 't', 'hd', 'xom', 'dis', 'vz', 'ko', 'mrk', 'cmcsa',
@@ -14,7 +16,29 @@ for security in securities:
    df[security] = ((yf.Ticker(security).history(start='2020-04-08', end = '2021-07-20').Close\
        - yf.Ticker(security).history(start='2020-04-08', end = '2021-07-20').Open)\
         /yf.Ticker(security).history(start='2020-04-08', end = '2021-07-20').Close) * 100
-   
-   #1/28/2021 - 08/16/2021
-  
-print(df)
+
+df.to_csv('intermediate.csv', index = True)
+df = pd.read_csv('intermediate.csv')
+
+df['Date'] = pd.to_datetime(df['Date'].apply(lambda x: x.split()[0]))
+df.set_index('Date', inplace = True)   
+
+df2 = pd.DataFrame()
+securities2 = ['gme', 'aal', 'aapl', 'amd', 'aph', 'bili', 'clov', 'dkng', 'ecor', 'meta', 'ino', 'jd', 'msft',
+               'mvis', 'cenn', 'plug', 'sndl', 'tlry', 'tsla', 'wkhs', 'zm']
+for security in securities2:
+   df2[security] = ((yf.Ticker(security).history(start='2021-01-28', end = '2021-08-17').Close\
+       - yf.Ticker(security).history(start='2021-01-28', end = '2021-08-17').Open)\
+        /yf.Ticker(security).history(start='2021-01-28', end = '2021-08-17').Close) * 100
+
+#modify date column and set back to index
+df2.to_csv('intermediate', index = True)
+df2 = pd.read_csv('intermediate.csv')
+
+df2['Date'] = pd.to_datetime(df2['Date'].apply(lambda x: x.split()[0]))
+df2.set_index('Date', inplace = True)
+
+#save to csv 
+df.to_csv('yahoo_stock_1.csv')
+df.to_csv('yahoo_stock_2.csv')
+
